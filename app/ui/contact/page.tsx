4e4d6dc567d52c4465153c5c203e2fb5dfbf757c
F.I.N <77541419+fathularifin-6800@users.noreply.github.com>
2024-01-05 04:17:45 +0800

    Update page.tsx

--- app/ui/contact/page.tsx
+++ app/ui/contact/page.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
     const buttonContact = 'Follow me'
     return ( 
         <main className="flex justify-between items-center flex-col m-20">
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-96 bg-neutral-900 shadow-xl">
             <div className="card-body items-center text-center">
                 <h2 className="card-title">F.I.N</h2>
                 <p>follow me to know me more</p>
